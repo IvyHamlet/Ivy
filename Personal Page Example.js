a31c99fccbd3283957251b4d917397ec889fe18f
IvyHamlet <131323622+IvyHamlet@users.noreply.github.com>
2023-04-20 11:13:44 +0900

    Add files via upload
    First upload.

---
+++ Personal Page Example.js
@@ -0,0 +1,50 @@
+<!DOCTYPE html>  
+<html>  
+<head>  
+	<title>My Personal Page</title>  
+	<style>  
+		body {  
+			font-family: Arial, sans-serif;  
+			background-color: #f2f2f2;  
+		}
+
+		.header {  
+			background-color: #333;  
+			color: white;  
+			padding: 20px;  
+		}
+
+		.content {  
+			padding: 20px;  
+		}
+
+		.footer {  
+			background-color: #333;  
+			color: white;  
+			padding: 20px;  
+		}  
+	</style>  
+</head>  
+<body>  
+	<header class="header">  
+		<h1>My Personal Page</h1>  
+	</header>
+
+	<div class="content">  
+		<h2>Personal Information</h2>  
+		<p>Name: John Doe</p>  
+		<p>Address: 123 Main St.</p>  
+		<p>Phone: (123) 456-7890</p>
+
+		<h2>About Me</h2>  
+		<p>John is a software developer who loves to code and explore new technologies. He is also passionate about photography and enjoys taking pictures of nature and animals.</p>
+
+		<h2>Avatar</h2>  
+		<img src="avatar.jpg" alt="My Avatar">  
+	</div>
+
+	<footer class="footer">  
+		&copy; 2023 John Doe. All rights reserved.  
+	</footer>  
+</body>  
+</html>  
